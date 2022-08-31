d2f2ef9cb129d99ad0803e4cb71a77be5c635eec
greze12 <112576574+greze12@users.noreply.github.com>
2022-08-31 23:29:25 +0530

    Add files via upload

---
+++ Leetcode0.py
@@ -0,0 +1,16 @@
+
+
+
+def addnum(self, l1, l2, c=0):
+    val = l1.val + l2.val +c
+    c = val//10
+    ret = ListNode(val%10)
+
+    if(l1.next != None or l2.next != None or c != 0):
+        if l1.next == None:
+            l1 = ListNode(0)
+            if l2.next == None:
+                l2 = ListNode(0)
+                ret.next = self.addnum(l1.next , l2.next , c)
+                return ret
+
